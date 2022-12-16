bc446486a61de96c003537e6a5c3b054c91a19a6
Cloves José <clovesjsilva8@gmail.com>
2022-12-16 19:50:14 -0300

    criando componente botao

--- web-designer/src/components/menu/menu.jsx
+++ web-designer/src/components/menu/menu.jsx
@@ -2,22 +2,25 @@ import React from "react";
 import { Fragment } from "react";
 import './menu.css'
 
+import Button from '../button/button'
+
 export default props => {
     return (
         <Fragment>
             <div className="container">
                 <div className="headerTitle">
-                    <div>
-                        <h3>SOCIONET</h3>
-                    </div>
+                    <h3>SOCIONET</h3>
                 </div>
                 <div className="optionsMenu">
-                    <div>Home</div>
-                    <div>About Us</div>
-                    <div>Spaces</div>
-                    <div>Portfolio</div>
-                    <div>Contact Us</div>
-                    <div>Pages</div>
+                    <a href="#">Home</a>
+                    <a href="#">About Us</a>
+                    <a href="#">Spaces</a>
+                    <a href="#">Portfolio</a>
+                    <a href="#">Contact Us</a>
+                    <a href="#">Pages</a>
+                </div>
+                <div>
+                    <Button title="GET STARTED"/>
                 </div>
             </div>
         </Fragment>
